451cfbe1734f2b979366dcb565350aeb9eb8b934
Ionut Alexuc <mxc.consulting@gmail.com>
2020-04-26 15:25:04 +0300

    Fix coding standards

--- src/Plugin/views/field/WSViewsField.php
+++ src/Plugin/views/field/WSViewsField.php
@@ -2,12 +2,10 @@
 
 namespace Drupal\ws_views_field\Plugin\views\field;
 
-
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Render\Markup;
 use Drupal\views\Plugin\views\field\FieldPluginBase;
 use Drupal\views\ResultRow;
-use Drupal\webform\Entity\Webform;
 
 
 /**
@@ -20,15 +18,17 @@ use Drupal\webform\Entity\Webform;
 class WSViewsField extends FieldPluginBase {
 
   /**
-   * @{inheritdoc}
+   * Empty query method.
    */
   public function query() {
     // Leave empty to avoid a query on this field.
   }
 
   /**
-   * Define the available options
+   * Define the available options.
+   *
    * @return array
+   *   Return defined options.
    */
   protected function defineOptions() {
     $options = parent::defineOptions();
@@ -41,18 +41,18 @@ class WSViewsField extends FieldPluginBase {
    * Provide the options form.
    */
   public function buildOptionsForm(&$form, FormStateInterface $form_state) {
-    $form['token'] = array(
+    $form['token'] = [
       '#title' => $this->t('Token'),
       '#type' => 'textarea',
       '#default_value' => $this->options['token'],
       '#description' => $this->t('Please enter one or more tokens from available list of tokens.'),
-    );
+    ];
 
     // Available tokens.
-    $form['ws_tokens'] = array(
+    $form['ws_tokens'] = [
       '#type' => 'details',
       '#title' => $this->t('Webform Submissions Tokens'),
-    );
+    ];
 
     // Add webform tokens.
     $webforms = $this->getWebforms();
@@ -63,10 +63,10 @@ class WSViewsField extends FieldPluginBase {
     module_load_include('inc', 'webform', 'webform.tokens');
     $webform_submission_tokens = webform_token_info();
 
-    $form['ws_tokens']['ws_use_token'] = array(
+    $form['ws_tokens']['ws_use_token'] = [
       '#type' => 'details',
       '#title' => $this->t('Advanced use of tokens'),
-    );
+    ];
 
     $form['ws_tokens']['ws_use_token']['info'] = array(
       '#markup' => $webform_submission_tokens['tokens']['webform_submission']['values']['description'],
@@ -76,11 +76,11 @@ class WSViewsField extends FieldPluginBase {
   }
 
   /**
-   * @{inheritdoc}
+   * Render function.
    */
   public function render(ResultRow $values) {
     // Get all webform submission entities used in this view.
-    $webfom_submission_entities = array();
+    $webfom_submission_entities = [];
     if (get_class($values->_entity) == 'Drupal\webform\Entity\WebformSubmission') {
       $webfom_submission_entities[$values->_entity->bundle()] = $values->_entity;
     }
@@ -102,9 +102,9 @@ class WSViewsField extends FieldPluginBase {
       // Create the real  webform submission.
       $token = str_replace($bundle, "webform_submission:values", $token);
 
-      $token = $token_service->replace($token, array(
-        'webform_submission' => $webform_submission_entity,
-      ));
+      $token = $token_service->replace($token, [
+        'webform_submission' => $webform_submission_entity
+      ]);
     }
 
     return Markup::create($token);
@@ -112,7 +112,8 @@ class WSViewsField extends FieldPluginBase {
 
   /**
    * Get existing webforms.
-   * @return Webform[]
+   * @return \Drupal\webform\Entity\Webform[]
+   *   All available webforms.
    */
   private function getWebforms() {
     // Get all existing webforms.
